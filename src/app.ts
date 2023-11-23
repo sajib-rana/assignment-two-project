3a7240bd5fd1b543ee9973beb0e94e880fc1c9b8
sajib-rana <sajibrana444@gmail.com>
2023-11-23 09:00:21 +0600

    'update'

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express'
+
 const app : Application = express()
-const port = 3000
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
